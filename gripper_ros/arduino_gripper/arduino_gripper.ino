3cff12e4736e2944056d3eeb30e61458ee3be397
Dmitry Devitt <devittdv@gmail.com>
2020-03-15 11:54:07 +0300

    added arduino gripper

---
+++ gripper_ros/arduino_gripper/arduino_gripper.ino
@@ -0,0 +1,62 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+String inString = "";    // string to hold input
+
+int minVal = 50;
+int maxVal = 145;
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(90);              // tell servo to go to position in variable 'pos'
+ 
+ while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // send an intro:
+  Serial.println("\n\nString toInt():");
+  Serial.println();
+}
+
+void loop() {
+   // Read serial input:
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      int value = inString.toInt();
+      if (value == 1)
+      {
+      myservo.write(maxVal);              // tell servo to go to position in variable 'pos'  
+      }
+      if (value == 0)
+      {
+      myservo.write(minVal);              // tell servo to go to position in variable 'pos'  
+      }
+      
+      Serial.print("Value:");
+      Serial.println(inString.toInt());
+      Serial.print("String: ");
+      Serial.println(inString);
+      
+      // clear the string for new input:
+      inString = "";
+    }
+  }
+}
---
+++ gripper_ros/src/arduino_gripper_ros.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding=utf8
+
+"""
+Node for control adruino gripper.
+"""
+
+import serial
+import rospy
+# import threading
+
+from std_msgs.msg import Bool
+
+
+port = "/dev/ttyACM0"
+
+
+
+def set_cmd(state):
+    global ser
+    """
+    set
+    :param state:
+    :return:
+    """
+    if state is False:
+        ser.write("0" + '\r\n')
+    if state is True:
+        ser.write("1" + '\r\n')
+
+def command_clb(data):
+    print ("Set gripper state:" + data.data)
+    set_cmd(data.data)
+
+
+if __name__ == '__main__':
+    """
+    Main node
+    """
+    # Init node
+    rospy.init_node('arduino_gripper_ros', anonymous=True)
+    port = rospy.get_param("~port", port)
+    # Subscriber
+    rospy.Subscriber("/gripper", Bool, command_clb)
+
+    # init serial
+    ser = serial.Serial(port, 9600)
+    if ser.is_open == False:
+        ser.open()
+    else:
+        print ("ERROR: serial is close!")
+        exit()
+    # # get data in external thread
+    # thread = threading.Thread(target=getLidarData)
+    # thread.daemon = True
+    # thread.start()
+    # loop
+    try:
+        rospy.spin()
+        ser.close()
+
+    except rospy.ROSInterruptException:
+        if ser != None:
+            ser.close()
+    except:  # Ctrl+C
+        if ser != None:
+            ser.close()
